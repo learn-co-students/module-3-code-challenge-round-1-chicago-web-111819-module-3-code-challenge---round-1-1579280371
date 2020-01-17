e530b32be8b773a35a1a68bd31e0d2ec6870afb8
github username <lluisguardiola.h@gmail.com>
2020-01-17 11:46:31 -0600

    like feature frontend done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+let imageId = 4416 //Enter the id from the fetched image here
+const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+const likeURL = `https://randopic.herokuapp.com/likes/`
+const commentsURL = `https://randopic.herokuapp.com/comments/`
 
-  let imageId = 1 //Enter the id from the fetched image here
+const main = () => {
+	document.addEventListener('DOMContentLoaded', () => {
+		console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+		fetchImage(imageURL)
+		likeImg()
+	  })
+}
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+const fetchImage = (imageURL) => {
+	fetch(imageURL)
+		.then(resp => resp.json())
+		.then(imgObj => renderImage(imgObj))
+}
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+const renderImage = (imgObj) => {
+	const imgCardDiv = document.querySelector('div#image_card')
+	
+	const imgTag = imgCardDiv.children[0]
+	imgTag.src = imgObj.url
 
-})
+	const h4 = imgCardDiv.children[1]
+	h4.innerText = imgObj.name
+
+	const likesSpan = imgCardDiv.children[2].firstElementChild
+	likesSpan.innerText = imgObj.like_count
+}
+
+const likeImg = () => {
+	const likeBtn = document.querySelector('#like_button')
+	likeBtn.addEventListener('click', (e) => {
+		const likesCount = e.target.previousElementSibling.firstElementChild
+		const newLikeCount = parseInt(likesCount.innerText)+1
+
+		likesCount.innerText = newLikeCount
+
+		reqObj = {
+			method: "PATCH",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json"
+			},
+			body: JSON.stringify({like_count: newLikeCount})
+		}
+
+		fetch(likeURL, reqObj) //WHAT ROUTE TO USE FOR PATCH?!!?!
+	})
+}
+
+
+main()
