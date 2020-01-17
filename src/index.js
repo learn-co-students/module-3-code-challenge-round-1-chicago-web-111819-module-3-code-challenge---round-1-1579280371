bc8c6173d6c765994bcb450fe1bc9cfa5bb02176
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-17 11:39:00 -0600

    optimistically renders likes and comments

--- src/index.js
+++ src/index.js
@@ -1,48 +1,80 @@
 function main() {
   document.addEventListener('DOMContentLoaded', () => {
-    console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+    console.log('%c DOM Content Loaded and Parsed!', 'color: magenta');
 
     let imageId = 4420 //Enter the id from the fetched image here
 
-    const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+    const imageURL = `https://randopic.herokuapp.com/images/${imageId}`;
 
-    const likeURL = `https://randopic.herokuapp.com/likes/`
+    const likeURL = `https://randopic.herokuapp.com/likes/`;
 
-    const commentsURL = `https://randopic.herokuapp.com/comments/`
+    const commentsURL = `https://randopic.herokuapp.com/comments/`;
 
-    fetchImage(imageURL)
+    fetchImage(imageURL);
+    likeListener();
+    commentSubmitter();
 
-  })
-}
+  });
+};
 
 function fetchImage(imageURL) {
   fetch(imageURL)
   .then(resp => resp.json())
-  .then(renderImage)
-}
+  .then(renderImage);
+};
 
 function renderImage(imageObj) {
-  const img = document.querySelector('img#image')
-  img.src = imageObj.url
-  img.dataset.id = imageObj.id
+  const img = document.querySelector('img#image');
+  img.src = imageObj.url;
+  img.dataset.id = imageObj.id;
 
-  const title = document.querySelector('h4#name')
-  title.innerText = imageObj.name
+  const title = document.querySelector('h4#name');
+  title.innerText = imageObj.name;
 
-  const likes = document.querySelector('span#likes')
-  likes.innerText = imageObj.like_count
+  const likes = document.querySelector('span#likes');
+  likes.innerText = imageObj.like_count;
 
-  imageObj.comments.forEach(renderComment)
-}
+  imageObj.comments.forEach( comment => renderComment(comment.content));
+};
 
 function renderComment(comment) {
-  const commentList = document.querySelector('ul#comments')
+  const commentList = document.querySelector('ul#comments');
 
-  const li = document.createElement('li')
-  li.dataset.id = comment.id
-  li.innerText = comment.content
+  const li = document.createElement('li');
+  li.innerText = comment;
 
-  commentList.append(li)
-}
+  commentList.append(li);
+};
 
-main()
+function likeListener() {
+  const button = document.querySelector('button#like_button');
+  button.addEventListener('click', () => {
+    renderLike();
+    postLike();
+  })
+};
+
+function renderLike() {
+  //optimistically renders likes
+  const likes = document.querySelector('span#likes');
+  likes.innerText = parseInt(likes.innerText) + 1;
+};
+
+function postLike() {
+
+};
+
+function commentSubmitter() {
+  const form = document.querySelector('form#comment_form')
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(e.target.comment.value);
+    //optimistically renders comment
+    const comment = e.target.comment.value;
+    renderComment(comment);
+    form.reset();
+  });
+  
+};
+
+main();
